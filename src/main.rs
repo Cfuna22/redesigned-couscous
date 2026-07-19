dec097e11582c346d02e0faa050941b81bdaa49f
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-20 02:07:15 +0300

    fixed: lint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-fn main() {
-    let age: u32 = 18;
-    let temperature: f64 = 40.6;
-    let num: i32 = -30;
-    let grade: char = 'A';
-    let is_active: bool = true;
-
-    println!("age {}, temperature {}, number {}, grade {}, is_active {}", age, temperature, num, grade, is_active);
-
-    // Compound type
-
-    // tuple
-    let person: (&str, u32) = ("maya", 19);
-    println!("{} is {} years old", person.0, person.1);
-    // array
-    let score: [u32; 3] = [23, 40, 8];
-    println!("the sore was {}", score[1]);
-}
-
 fn next() {
     greet("abel");
 }
