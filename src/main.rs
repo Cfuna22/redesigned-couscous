e508b9722ababd8553d4dfce2f3b0f03f9cead68
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-20 02:03:06 +0300

    fixed: lint

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,13 @@ fn main() {
     let is_active: bool = true;
 
     println!("age {}, temperature {}, number {}, grade {}, is_active {}", age, temperature, num, grade, is_active);
+
+    // Compound type
+
+    // tuple
+    let person: (&str, u32) = ("maya", 19);
+    println!("{} is {} years old", person.0, person.1);
+    // array
+    let score: [u32; 3] = [23, 40, 8];
+    println!("the sore was {}", score[1]);
 }
