c29d9925e98662ccbf35c4a336e6ec165c4a856b
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-20 01:48:47 +0300

    fixed: lint

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     let num: i32 = -30;
     let grade: char = 'A';
     let is_active: bool = true;
+
+    println!("age {}, temperature {}, number {}, grade {}, is_active {}", age, temperature, num, grade, is_active);
 }
