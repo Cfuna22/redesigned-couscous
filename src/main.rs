6bb69357477da88da3bffd01d013f254fdaa9a74
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-20 10:29:00 +0300

    fixed: lint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     greet("abel");
+    println!("{}",add(30, 40));
 }
 
 fn greet(name: &str) {
     println!("greetings, {}", name);
+}
+
+fn add(a: i32, b: i32) -> i32{
+    a + b // this is an expression, it's value automatically becomes the return value
 }
