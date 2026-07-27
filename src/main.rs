aefe1494bb144498f8f45b1a0754cedf8c66a654
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-27 22:45:46 +0300

    update project createion

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,5 @@ fn greet(name: &str) {
 }
 
 fn add(a: i32, b: i32) -> i32{
-    a + b // this is an expression, it's value automatically 
+    a + b // this is an expression, it's value automatically becomes the return value
 }
