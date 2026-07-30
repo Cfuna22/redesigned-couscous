c5fe20cd912eda71e002b6b612b18487793a6cbf
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-31 00:05:03 +0300

    update project createion

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ fn main() {
     let number = 6;
 
     if number % 2 == 0 {
-        println!()
+        println!("The number is even")
+    } else {
+        println!("The number is odd")
     }
 }
