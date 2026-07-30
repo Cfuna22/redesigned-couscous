e08bf49774360aae2909804b89f54a925d814fda
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-30 23:58:48 +0300

    update project createion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-fn main() {
-    greet("abel");
-    println!("{}",add(30, 40));
-}
+// fn main() {
+//     greet("abel");
+//     println!("{}",add(30, 40));
+// }
 
-fn greet(name: &str) {
-    println!("greetings, {}", name);
-}
+// fn greet(name: &str) {
+//     println!("greetings, {}", name);
+// }
 
-fn add(a: i32, b: i32) -> i32{
-    a + b // this is an expression, it's value automatically becomes the return value
-}
+// fn add(a: i32, b: i32) -> i32{
+//     a + b // this is an expression, it's value automatically becomes the return value
+// }
+
+
