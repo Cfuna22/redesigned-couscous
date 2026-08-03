d1ecfb7125ea060374df1e6b525c807b814aa752
Abel Sifuna <abelsifuna515@gmail.com>
2026-08-04 00:16:29 +0300

    fix lint

--- src/main.rs
+++ src/main.rs
@@ -53,14 +53,13 @@ fn main() {
 
     let ss = String::from("Hello");
     let len = calculate_length(&ss);
-    println!("the length of {} is {}", ss, len)
+    println!("the length of {} is {}", ss, len);
 
     let mut s = String::from("value");
 
     let r1 = &mut s;
-    let r2 = &mut s;
 
-    println!("{}, {}", r1, r2)
+    println!("{}", r1,)
 
 }
 
